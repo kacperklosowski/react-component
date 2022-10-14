e8993b226e570a26fb5907dbe34ca6fe86ba89ce
Kacper Klosowski <kacper.klosowski@opuscapita.com>
2022-10-14 13:27:49 +0200

    x

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ const sass = require('rollup-plugin-sass');
 const typescript = require('rollup-plugin-typescript2')
 
 module.exports = {
-  input: 'src/index.tsx',
+  input: 'src/index.ts',
   output: [
     {
       file: 'dist/index.js',
